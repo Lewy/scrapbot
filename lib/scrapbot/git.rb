62b25c7527d00f6b38df1c7d57b575e211173ad1
lewy <lewy313@gmail.com>
2011-03-17 20:04:55 +0100

    Switching beetween branches is working

--- lib/scrapbot/git.rb
+++ lib/scrapbot/git.rb
@@ -18,17 +18,35 @@ module Scrapbot
       unless File.directory?(git_directory)
         cd self.path_to_git
         `git init`
-        add_all
-        commit("Initial commit.")
+        touch ".gitignore"
+        push("Inital commit.")
       end
     end
 
     def add_all
+      cd self.path_to_git
       `git add .`
     end
 
     def commit(msg)
-      `git commit -am #{msg}`
+      cd self.path_to_git
+      `git commit -am "#{msg}"`
+    end
+
+    def push(msg)
+      cd self.path_to_git
+      add_all
+      commit(msg)
+    end
+
+    def find_branch(name)
+      cd self.path_to_git
+      `git checkout #{name}`
+    end
+
+    def create_branch(name)
+      cd self.path_to_git
+      `git checkout -b #{name}`
     end
     
     private
@@ -36,6 +54,6 @@ module Scrapbot
     def git_directory
       File.join(self.path_to_git + ".git")
     end
-
+    
   end
 end
--- lib/scrapbot/storage.rb
+++ lib/scrapbot/storage.rb
@@ -1,31 +1,40 @@
 require 'uri'
-require 'git'
+require_relative 'git'
 
 module Scrapbot
   class Storage
 
-    attr_accessor :magazine
+    attr_accessor :magazine, :repository
 
     def initialize
       self.magazine = []
-
+      self.repository = Git.new
+      repository.init
     end
 
     def save_all
+      self.magazine.each do |r|
+        repository.find_branch("master")
+        repository.create_branch(filename(r))
+      end
+
       self.magazine.each do |response|
+        repository.find_branch(filename(response))
         File.open(save_path(response),'wb') do |f|
           f.write(response.body)
         end
+        repository.push("Correcly added new version of #{filename(response)}")
       end
-      g = Git.new
-      g.init
     end
 
     private
 
     def save_path(response)
-      filename = URI.parse(response.effective_url).host
-      File.join(Settings[:path_to_git],"#{filename}.html")
+      File.join(Settings[:path_to_git],"#{filename(response)}.html")
+    end
+
+    def filename(response)
+      URI.parse(response.effective_url).host
     end
 
   end
